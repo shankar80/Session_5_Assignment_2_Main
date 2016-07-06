d85c03dd8b5ec587ac34a6fc72fdfc80f57ad1e9
shankar80 <shankarraopura2016@gmail.com>
2016-07-06 14:04:40 +0530

    Session_5_Assignment_2_Main
    Session_5_Assignment_2_Main

---
+++ intentimplement/app/src/main/java/in/dailytalent/www/intentimplement/MainActivity.java
@@ -0,0 +1,50 @@
+package in.dailytalent.www.intentimplement;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button vibrate_bt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+        vibrate_bt1=(Button)findViewById(R.id.vibrate_bt);
+
+        vibrate_bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 1);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
